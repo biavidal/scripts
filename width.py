67e7b9ef9d4e37b5ee51518fc5289e1fe6929818
Beatriz Vidal <127804056+biavidal@users.noreply.github.com>
2024-04-26 21:03:26 -0300

    width 2.0

--- width.py
+++ width.py
@@ -1,28 +1,39 @@
-width_copper = {
-    'A': 9,  # Metal 1
-    'B': 9,  # Data 2
-    'C': 12,  # Buyer 3
-    'D': 9,  # Metal 4
-    'E': 9,  # Data 5
-    'F': 12,  # Buyer 6
-    'G': 9,  # Metal 7
-    'H': 12,  # Data 8
-    'I': 9   # Buyer 9
-}
+import pandas as pd
 
-width_copper = {
-    'A': 20,  # Metal 1
-    'B': 9,  # Data 2
-    'C': 12,  # Buyer 3
-    'D': 20,  # Metal 4
-    'E': 9,  # Data 5
-    'F': 12,  # Buyer 6
-    'G': 20,  # Metal 7
-    'H': 9,  # Data 8
-    'I': 12   # Buyer 9
-}
+def ajustar_largura_colunas(df):
+    # Cria um dicionário para armazenar as larguras ajustadas
+    largura_colunas = {}
 
-for col, width in width.items():
-        worksheet.set_column(col + ':' + col, width)
+    # Itera sobre as colunas do DataFrame
+    for col in df.columns:
+        # Calcula a largura máxima de cada coluna
+        largura_maxima = max(df[col].astype(str).apply(len).max(), len(col))
         
-#parâmetros para passar(width)
+        # Adiciona 2 caracteres adicionais para uma margem
+        largura_ajustada = largura_maxima + 2
+        
+        # Armazena a largura ajustada para a coluna
+        largura_colunas[col] = largura_ajustada
+
+    return largura_colunas
+
+dados_mean = {"Data" : ['aaaaa', 'aaaaa', 'aaaaaa'],
+                  "": ['bbbbb', 'bbbbbb', 'bbbbbb'],
+                  "January": ['cccccc', 'cccccc', 'cccccc'],
+                  "February": ['ddddd', 'ddddd', 'ddddd'],
+                  "March": ['eeeeee', 'eeeeee', 'eeeee']}
+
+dados_rate = {"Tabela" : ['aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaaaaaaaa'],
+              "Rate": ['bbbbb', 'bbbbbb', 'bbbbbb', 'bbbbbbbb', 'bbbbbbb', 'bbbbbbb'],
+              "January": ['cccccc', 'cccccc', 'cccccc', 'cccccccc', 'ccccccc', 'cccccccc'],
+              "February": ['dddddddd', 'ddddd', 'ddddd', 'dddddddd', 'dddddddd', 'dddddddd'],
+              "March": ['eeeeee', 'eeeeee', 'eeeee', 'eeeeee', 'eeeeee', 'eeeeee']}
+
+df_mean = pd.DataFrame(dados_mean)
+df_rate = pd.DataFrame(dados_rate)
+ajustar_largura_colunas(df_mean)
+ajustar_largura_colunas(df_rate)
+# Imprime o DataFrame resultante
+with pd.ExcelWriter('dados_usd.xlsx', engine='xlsxwriter') as writer:
+    df_mean.to_excel(writer, index=False, startrow=2, sheet_name='Resumo')
+    df_mean.to_excel(writer, index=False, startrow=7, sheet_name='Resumo')
